95a49f961669f70cb3884505dd9e6f55b6aba4c4
Mesabe <bbgunbrits@gmail.com>
2021-02-24 10:25:57 +0200

    init

--- GetThisToWork.java
+++ GetThisToWork.java
@@ -5,5 +5,6 @@ public class GetThisToWork
         System.out.println("Should be first java on git");
         System.out.println("What now?");
         System.out.println("Should work");
+        System.out.println("Didnt work");
     }
 }
