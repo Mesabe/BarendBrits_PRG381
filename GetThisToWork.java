89db010be47c07a533c00686275b20b4955d9e9d
Mesabe <bbgunbrits@gmail.com>
2021-02-24 11:23:25 +0200

    Merge

--- GetThisToWork.java
+++ GetThisToWork.java
@@ -4,11 +4,8 @@ public class GetThisToWork
     {
         System.out.println("Should be first java on git");
         System.out.println("It Finaly worked");
-<<<<<<< HEAD
 
         
-=======
         //This is master
->>>>>>> 6cb413a6d7256d275acb77b6065c479b89200df6
     }
 }
