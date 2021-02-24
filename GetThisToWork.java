075d3890db9198dc3d10717a00a0698eaea6d357
Mesabe <bbgunbrits@gmail.com>
2021-02-24 10:32:50 +0200

    2de

--- GetThisToWork.java
+++ GetThisToWork.java
@@ -3,7 +3,7 @@ public class GetThisToWork
     public static void main(String[] args) 
     {
         System.out.println("Should be first java on git");
-        //I want to die
+        //I want to die badly
        
     }
 }
