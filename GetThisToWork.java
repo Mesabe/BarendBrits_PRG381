e6f17c811a6a2d0b6926147c07c56de3bf58d7a1
Mesabe <bbgunbrits@gmail.com>
2021-02-24 10:47:52 +0200

    Finaly there

--- GetThisToWork.java
+++ GetThisToWork.java
@@ -3,8 +3,6 @@ public class GetThisToWork
     public static void main(String[] args) 
     {
         System.out.println("Should be first java on git");
-        //I want to die badly
-        // so badly!!
-       // i give up
+        System.out.println("It Finaly worked");
     }
 }
